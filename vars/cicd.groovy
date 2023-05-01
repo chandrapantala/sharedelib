00d8d12afb8b37a8cb443af706f9b39b83b0891b
chandrapantala <131387671+chandrapantala@users.noreply.github.com>
2023-05-01 14:18:41 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,11 +2,8 @@ def newgit(repo)
 {
   git "https://github.com/intelliqittrainings/${repo}.git"
 }
-def newmaven()
+def newMaven()
 {
   sh 'mvn package'
 }
-def newdeploy()
-{
-   deploy "adapters: [tomcat9(credentialsId: '940cfe09-3905-4644-be26-658311726026', path: '', url: 'http://172.31.7.4:8080')], contextPath: 'testapp', war: '**/*.war'"
-}
+
