7113cc36f1d44d4ddd91f4f9048e75039a163db2
chandrapantala <131387671+chandrapantala@users.noreply.github.com>
2023-05-24 13:45:14 +0530

    Create cicd1.groovy
    sai

---
+++ vars/cicd1.groovy
@@ -0,0 +1,12 @@
+def newGit(repo)
+{
+  git "${repo}"
+}
+def newMaven()
+{
+  sh 'mvn package'
+}
+def newDeploy(workspace,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
