95ab43c5373640d682dfb854bdbd25176a47f25e
chandrapantala <131387671+chandrapantala@users.noreply.github.com>
2023-05-24 12:47:52 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,5 +8,6 @@ def newMaven()
 }
 def newDeploy(jobname,ipaddress,context)
 {
-   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${contest}.war"
+   sh "scp /var/lib/jenkins/workspace/'${jobname}'/webapp/target/webapp.war ubuntu@'${ipaddress}':/var/lib/tomcat9/webapps/'${contest}'.war"
 }
+
