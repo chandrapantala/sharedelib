fe447917c920b00f176cf3dc3c902a814317cb32
chandrapantala <131387671+chandrapantala@users.noreply.github.com>
2023-07-12 11:03:28 +0530

    Update cicd2.groovy

--- vars/cicd2.groovy
+++ vars/cicd2.groovy
@@ -6,4 +6,9 @@ def Maven()
 {
   sh 'mvn package'
 }
+def Deploy()
+{
+  sh "deploy adapters: [tomcat9(credentialsId: 'b421bc98-8b7a-4207-bf87-f03a9e1bb01c', path: '', url: 'http://172.31.89.185:8080')], contextPath: 'testapp', war: '**/*.war'"
+}
+
 
