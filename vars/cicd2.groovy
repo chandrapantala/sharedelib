7b5048458b60eb22eb015f3b891ca1f8d0ca9383
chandrapantala <131387671+chandrapantala@users.noreply.github.com>
2023-07-12 11:38:12 +0530

    Update cicd2.groovy

--- vars/cicd2.groovy
+++ vars/cicd2.groovy
@@ -10,5 +10,9 @@ def Deploy()
 {
   deploy adapters: [tomcat9(credentialsId: 'b421bc98-8b7a-4207-bf87-f03a9e1bb01c', path: '', url: 'http://172.31.89.185:8080')], contextPath: 'testapp', war: '**/*.war'
 }
+def test()
+{
+  sh 'java -jar '
+}
 
 
