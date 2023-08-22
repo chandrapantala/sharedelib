fb6b4b67566f9392d30238366b2a8ab2064524b0
chandrapantala <131387671+chandrapantala@users.noreply.github.com>
2023-08-22 12:19:30 +0530

    Create cicd3.groovy

---
+++ vars/cicd3.groovy
@@ -0,0 +1,16 @@
+def newGit(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}.git"
+}
+def newMaven()
+{
+  sh 'mvn package'
+}
+def newDeploy(workspace,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+def newtest(workspace)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${workspace}/testing.jar"
+}
