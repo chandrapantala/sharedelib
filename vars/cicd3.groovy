f1dd916d7c3d330902f049dd2aa466ac60b2bc64
chandrapantala <131387671+chandrapantala@users.noreply.github.com>
2023-08-22 13:11:02 +0530

    Update cicd3.groovy

--- vars/cicd3.groovy
+++ vars/cicd3.groovy
@@ -6,11 +6,14 @@ def newMaven()
 {
   sh 'mvn package'
 }
-def newDeploy(workspace,ip,context)
+def newDeploy()
 {
-  sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+  deploy adapters: [tomcat9(credentialsId: '66b4205e-de87-47af-bf21-0bbff285218f', path: '', url: 'http://172.31.40.3:8080')], contextPath: 'tet', war: '**/*.war'
 }
 def newtest(workspace)
 {
   sh "java -jar /var/lib/jenkins/workspace/${workspace}/testing.jar"
 }
+def newDepv()
+{
+  deploy adapters: [tomcat9(credentialsId: '66b4205e-de87-47af-bf21-0bbff285218f', path: '', url: 'http://172.31.33.19:8080')], contextPath: 'proda', war: '**/*.war'
